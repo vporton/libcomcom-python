e4e507302667dd8e0f245dc29c2c615e275026c4
Victor Porton <porton@narod.ru>
2018-01-31 18:35:09 +0200

    procedural interface created

--- comcom/low_level.py
+++
@@ -1,4 +0,0 @@
-from _low_level import ffi
-
-lib = ffi.dlopen('comcom')
-we = ffi.dlopen(None)
---
+++ comcom/procedural.py
@@ -0,0 +1,24 @@
+import ctypes
+import os
+
+from _low_level import ffi
+
+lib = ffi.dlopen('comcom')
+we = ffi.dlopen(None)
+
+def init():
+    if we.libcomcom_python_init() == -1:
+        raise OSError(ctypes.get_errno())
+
+def run_command(input, file, argv, env=os.environ, timeout=-1):
+    output = ffi.new("char **")
+    output_len = ffi.new("size_t *")
+    argv2 = ffi.new("char **", [ffi.new("char[]", v) for v in argv+[None]])
+    envp2 = ffi.new("char **", [ffi.new("char[]", "%s=%s" % p) for p in env.items()])
+    if lib.libcomcom_run_command(input, len(input), output, output_len, file, argv2, envp2, timeout) == -1:
+        raise OSError(ctypes.get_errno())
+    return ffi.string(output)
+
+def terminate():
+    if lib.libcomcom_terminate() == -1:
+        raise OSError(ctypes.get_errno())
