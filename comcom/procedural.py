b54a3d87c6f961ea9e6aebc61a4449800f724f31
Victor Porton <porton@narod.ru>
2018-01-31 20:23:17 +0200

    testing (currently segfaults)

--- comcom/procedural.py
+++ comcom/procedural.py
@@ -3,10 +3,10 @@ import os
 
 from _low_level import ffi
 
-lib = ffi.dlopen('/usr/local/lib/libcomcom.so')
+lib = ffi.dlopen('libcomcom.so')
 we = ffi.dlopen(None)
 
-__all__ = ['init', 'run_command', 'terminate']
+__all__ = ['init', 'run_command', 'destroy']
 
 def init():
     if we.libcomcom_python_init() == -1:
@@ -15,8 +15,8 @@ def init():
 def run_command(input, file, argv, env=os.environ, timeout=-1):
     output = ffi.new("char **")
     output_len = ffi.new("size_t *")
-    argv2 = ffi.new("char **", [ffi.new("char[]", v) for v in argv+[None]])
-    envp2 = ffi.new("char **", [ffi.new("char[]", "%s=%s" % p) for p in env.items()])
+    argv2 = ffi.new("char *[]", [ffi.new("char[]", v) for v in argv] + [ffi.new("char*")])
+    envp2 = ffi.new("char *[]", [ffi.new("char[]", "%s=%s" % p) for p in env.items()] + [ffi.new("char*")])
     if lib.libcomcom_run_command(input, len(input), output, output_len, file, argv2, envp2, timeout) == -1:
         raise OSError(ctypes.get_errno())
     return ffi.string(output[0], output_len[0])
