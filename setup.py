3161490902ff058d412af0b40ffc13c19d21c873
Victor Porton <porton@narod.ru>
2018-01-31 10:29:48 +0200

    initial repository

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='libcomcom-python',
+    version='0.0.1',
+    packages=[''],
+    url='libcomcom-python',
+    license='AGPLv3',
+    author='Victor Porton',
+    author_email='porton@narod.ru',
+    description='LibComCom Python wrapper'
+)
